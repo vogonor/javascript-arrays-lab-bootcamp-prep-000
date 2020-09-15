ec27172f79513276a4515c75c8853f836bfef00f
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-15 11:13:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var kittens = [] //define your array here
 
 // Add your functions and code here
-function kittens = ["Milo", "Otis", "Garfield"]
-  
-  
+function appendKittens ()
+{
+  var kittens == ["Milo", "Otis", "Garfield"]
 }
