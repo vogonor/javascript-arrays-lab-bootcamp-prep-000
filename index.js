fc9842435bc59a08dcdea516d18d2e3b3ca0ffec
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-15 11:20:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ var kittens = [] //define your array here
 // Add your functions and code here
 function appendKitten()
 {
-  var kittens = ["Milo", "Otis", "Garfield"]
+  kittens.push()
+//  var kittens = ["Milo", "Otis", "Garfield"]//
+    kittens.unshift("Garfield")
+    kittens.unshift("Otis")
+    Kittens.unshift("Milo")
+    
+    return kittens
   
-  return kittens
 }
 
